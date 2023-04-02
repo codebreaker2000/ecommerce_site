a323b62b8d2771a051b383355da571ed5b046946
Bharat <bharat1910109@akgec.ac.in>
2023-04-02 21:11:17 +0530

    static files added

--- ecomsite/ecomsite/settings.py
+++ ecomsite/ecomsite/settings.py
@@ -37,12 +37,15 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'whitenoise.runserver_nostatic',
+    'django.contrib.staticfiles',    
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
--- ecomsite/ecomsite/settings_rail.py
+++ ecomsite/ecomsite/settings_rail.py
@@ -18,3 +18,8 @@ DATABASES = {
         'OPTIONS': {'sslmode':'require'},
     }
 }
+import os
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'shop/static')]
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
