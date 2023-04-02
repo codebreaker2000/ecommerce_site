3ce34811c92fdfdc318d729ef1c2ca5192d8f296
Bharat <bharat1910109@akgec.ac.in>
2023-04-02 18:42:36 +0530

    deployment1

---
+++ ecomsite/ecomsite/settings_rail.py
@@ -0,0 +1,3 @@
+from ecomsite.settings import *
+from decouple import config
+SECRET_KEY = config('SECRET_KEY')
--- ecomsite/shop/admin.py
+++ ecomsite/shop/admin.py
@@ -2,5 +2,13 @@ from django.contrib import admin
 from .models import Products,Order
 
 # Register your models here.
-admin.site.register(Products)
+
+class Product_items(admin.ModelAdmin):
+    list_display=('title','price','discount_price','category','description')
+    search_fields=('title',)
+
+admin.site.site_header="E-commerce Site"
+admin.site.site_title="ABC Shop"
+admin.site.index_title="Manage ABC Shop"
+admin.site.register(Products,Product_items)
 admin.site.register(Order)
